76bc1269524c115c8302da05e990187abb0fb379
Vlad Deynega <moneabraams@gmail.com>
2020-07-16 18:02:20 +0700

    first
    first commit

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public bool StartStatus = false;
+    public void StartGame()
+    {
+        Camera.main.transform.position = new Vector3(5f, 7f, 0);
+        StartStatus = true;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlaeyrMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaeyrMove : MonoBehaviour
+{
+    public GameObject obj;
+    public float MoveSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float h = Input.GetAxis("Horizontal");
+        //float xPos = h * speed;
+        //obj.transform.position = new Vector3(xPos, 0.35f, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            obj.transform.Translate(-Vector3.forward * MoveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            obj.transform.Translate(-Vector3.right * MoveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            obj.transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
+
+    }
+}
