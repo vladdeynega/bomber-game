d7886c8522b7034211f7c698fd6e00e48070cde1
Дмитрий <lala@mail.ru>
2020-07-16 14:57:55 +0300

    gg
    Voobche huinia

---
+++ Assets/Scripts/Bombs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bombs : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] bombs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bombs[0], player.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    //public GameObject Canvas;
+    public float Speed_Cam = 5f;
+    //private CanvasButtons _c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // _c = Canvas.GetComponent<CanvasButtons>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if (Canvas.GetComponent<>)
+        transform.Rotate(0, Speed_Cam * Time.deltaTime, 0);
+    }
+}
